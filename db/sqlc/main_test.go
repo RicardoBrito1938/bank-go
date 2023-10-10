88212b0bd53a86200b778a4f424cae6c3daf55a2
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2023-10-10 22:46:13 +0100

    chore: unit test for the database connection

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:password@localhost:5432/simple_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	testQueries = New(conn)
+
+	code := m.Run()
+	os.Exit(code)
+}
