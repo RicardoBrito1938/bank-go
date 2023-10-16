f6f8aba244b523421cdf7a3a149b424688dd1128
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2023-10-16 22:42:25 +0100

    fix: push correct ammount

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -94,8 +94,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		if arg.FromAccountID < arg.ToAccountID {
 			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
 		} else {
-			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, arg.Amount)
-
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
 		}
 		return nil
 	})
