1c4c24b873fb4bede71416eef98ed996d199f003
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2023-10-23 22:21:25 +0100

    feature: implement gin

---
+++ api/server.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	db "bank.sqlc.dev/app/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves http requests the api
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server on a specific address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"bank.sqlc.dev/app/api"
+	db "bank.sqlc.dev/app/db/sqlc"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:password@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = ":8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+}
